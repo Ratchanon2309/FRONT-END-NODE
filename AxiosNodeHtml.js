b0c3c918c8af08500a8849c7f2c3b0f175401b4c
Ratchanon2309 <euroeat25@gmail.com>
2023-09-19 18:59:12 +0700

    up

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "node50275-ratchanon.proen.app.ruk-com.cloud:11480";
+const base_url = "http://10.104.9.209";
 
 // Set the template engine
 app.set('view engine', 'ejs');
@@ -85,6 +85,6 @@ app.get("/delete/:id", async (req, res) => {
     }
 });
 
-app.listen(5500, () => {
-    console.log('Server started on port 5500');
+app.listen(8080, () => {
+    console.log('Server started on port 8080');
 });
